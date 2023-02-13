b19297e4f6d072869396fedd524ebb2f17bef8be
Thomaz Bittencourt <thomazbittencourt@Mac-Mini.local>
2023-02-13 16:13:16 -0300

    fix(category page) fix layout for mobile devices

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -10,9 +10,9 @@ const route = useRoute()
 const productsStore = store.state.products
 const query = ref(route.params.name)
 const sortOptions = ['Nome', 'Preço', 'Avaliação']
-const sortType = ['asc', 'desc']
 const sortSelected = ref('')
 const sortTypeSelected = ref ('')
+const innerWidth = window.innerWidth
 onMounted(async () => {
     document.title = route.params.name as string;
     await store.commit('getProductByCategory', route.params.name)
@@ -50,9 +50,28 @@ function handleSortProducts(sort: string){
 </script>
 
 <template>
-    <div class=" flex justify-between items-center p-5">
+    <div v-if="innerWidth > 512" class=" flex justify-between items-center p-5">
         <h2 class="text-2xl font-medium underline">{{ route.params.name }}</h2>
         <div class="flex items-center gap-2">
+            <span>Ordenar por: </span>
+            <div class="bg-primary flex justify-between overflow-hidden rounded-2xl w-80  items-center divide-x divide-secondary">
+                <div v-for="sortOption in sortOptions" class="w-1/3 flex justify-center">
+                    <button @click="handleSortProducts(sortOption)" :class="{'bg-secondary':sortSelected === sortOption }"  class="text-white text-lg font-medium h-full w-full">{{sortOption}}</button>
+                </div>
+            </div>
+            <div v-if="sortSelected !== ''" class="bg-primary flex justify-between overflow-hidden rounded-2xl w-40  items-center divide-x divide-secondary">
+                <div  class="w-1/2 flex justify-center">
+                    <button @click="handleSortType('asc')" :class="{'bg-secondary':sortTypeSelected === 'asc' }"  class="text-white text-lg font-lg h-full w-full flex justify-center "> <ArrowUpIcon class="text-white"/> </button>
+                </div>
+                <div  class="w-1/2 flex justify-center">
+                    <button @click="handleSortType('desc')" :class="{'bg-secondary':sortTypeSelected === 'desc' }"  class="text-white text-lg font-lg h-full w-full flex justify-center"> <ArrowDownIcon class="text-white" /> </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div v-else  class=" flex flex-col gap-5 justify-between items-center p-5">
+        <h2 class="text-2xl font-medium underline">{{ route.params.name }}</h2>
+        <div class="flex flex-col items-center gap-2">
             <span>Ordenar por: </span>
             <div class="bg-primary flex justify-between overflow-hidden rounded-2xl w-80  items-center divide-x divide-secondary">
                 <div v-for="sortOption in sortOptions" class="w-1/3 flex justify-center">
